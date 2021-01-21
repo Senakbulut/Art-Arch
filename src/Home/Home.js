43f2a2aae34dcd1761040ffe2a7478234b01504f
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-01-21 23:06:13 +0300

    create frame

---
+++ src/Home/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <div className="container">
+        <div className="container-line">
+          <div className="line">
+            <div className="header">
+              <h1>ART & ARCH</h1>
+            </div>
+            
+            <div className="picture1">
+          
+            </div>
+            <div className="picture2"></div>
+            <p>The Most Modern and Interesting Buildings in the World</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
