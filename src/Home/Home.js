255d3517797e8421753f2498bcf71f85448d7939
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-01-23 21:52:02 +0300

    fade effect added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-
+import {Fade} from 'react-reveal';
 function Home() {
   return (
     <div>
@@ -10,13 +10,17 @@ function Home() {
             <div className="header">
               <h1>ART & ARCH</h1>
             </div>
-            
-             <div className="picture1">
+            <Fade left duration={2000}>
+            <div className="picture1">
             <img src="images/image3.png" alt=""/>
             </div>
+            </Fade>
+            <Fade right duration={2000}>
             <div className="picture2">
               <img src="images/image2.png" alt=""/>
             </div>
+            </Fade>
+            
             <div className="text">
             <p>The Most Modern and Interesting Buildings in the World</p>
             </div> 
